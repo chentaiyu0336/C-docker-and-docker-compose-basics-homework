689c3eff3c0136bf5752a7a85b5a112521af8c60
Taiyu Chen <taiyu.chen@Taiyus-MacBook-Pro.local>
2020-09-23 22:06:48 +0800

    [cty]fix controller

--- bob/src/main/java/com/thoughtworks/capability/gtb/bob/api/BobController.java
+++ bob/src/main/java/com/thoughtworks/capability/gtb/bob/api/BobController.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.capability.gtb.bob.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BobController {
+    @GetMapping("/hello")
     public String hello() {
         return "Hello from Bob!";
     }
